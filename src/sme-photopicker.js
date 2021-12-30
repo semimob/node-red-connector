7cb3dfcd50d715c24b93bd5627cb4a0a46336aac
Phuc Nguyen <nkhhphuc@gmail.com>
2021-12-30 21:43:50 +0700

    Add file picker and photo picker message nodes.

---
+++ src/sme-photopicker.js
@@ -0,0 +1,48 @@
+"use strict";
+
+module.exports = function (RED) {
+
+    function SmeFilePickerNode(config) {
+        RED.nodes.createNode(this, config);
+
+        this.name = config.name;
+        this.title = config.title;
+        this.buttonText = config.buttonText;
+        this.required = config.required;
+        this.formStatus = config.formStatus;
+
+        var node = this;
+
+        node.on('input', function (msg, send, done) {
+            send = send || function () { node.send.apply(node, arguments) };
+
+            var m = typeof (msg.payload) == 'object' ? (msg.payload || {}) : {};
+
+            m.Type = 'chat';
+            m.TypeID = '457d1d4f-c982-4caf-bcc4-4b435860efa3';
+            m.Body = node.title || node.name || m.Body;
+            m.FormReference = m.FormReference || node.name;
+            m.FormStatus = node.formStatus ? 1 : 0;
+            m.FormItems = m.FormItems || [];
+
+            m.FormItems.push({
+            	FormTypeID: 'bc273581-0431-49d5-a621-22c2ad72a853',
+                FormTypeConfig: {
+                    Title: node.title,
+                    ButtonText: node.buttonText
+                },
+                FormValue: node.value,
+                FormRequired: m.FormItems.length == 0 || (node.required == 1),
+                FormReference: node.name
+            });
+            
+            msg.payload = m;
+
+            send(msg, false);
+
+            done && done();
+        });
+    };
+
+    RED.nodes.registerType("sme-photopicker", SmeFilePickerNode);
+};
